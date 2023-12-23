3135dd5638273c10e3c746e9a848192993cc8a98
ilyben2004 <ilyasbennajeh@gmail.com>
2023-12-23 18:08:55 +0100

    yes

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
 import java.util.*;
-interface  i {}
-  class  A{
-
-  }
-class l extends  A implements  i{};
-
 
 public class Main {
 
@@ -12,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // Creating sample students
-     
+
 
 
 
